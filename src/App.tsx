64f384b44178e3be800ceb442ff0719e3a9c9ef7
Danilo <danilobellanda2b@gmail.com>
2022-11-25 14:50:16 -0300

    commit correcao de errro

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import { HashRouter as Router , Routes, Route} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './assets/Components/Pages/Home/Home'
